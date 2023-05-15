415c0a012477c338f3d7b3f737208008eff33372
yobra254 <brayodhis071@gmail.com>
2023-05-15 20:15:55 +0300

    Creating the UI for Questions And Ans

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +11,23 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('QuestionsScreen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The Question....',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: 'Answer Text ....1', onTap: () {}),
+          AnswerButton(answerText: 'Answer Text ....2', onTap: () {}),
+          AnswerButton(answerText: 'Answer Text ....3', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
