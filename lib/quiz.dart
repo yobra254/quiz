bc3a090e01835e6eeff6cde6853a5603b22c2900
yobra254 <brayodhis071@gmail.com>
2023-06-05 22:07:22 +0300

    Results Screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -56,7 +56,9 @@ class _QuizState extends State<Quiz> {
     }
 
     if (activeScreen == 'results-screen') {
-      screenWidget = const ResultScreen();
+      screenWidget = ResultScreen(
+        chosenAnswers: selectedAnswers,
+      );
     }
 
     return MaterialApp(
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -3,7 +3,12 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  const ResultScreen({
+    super.key,
+    required this.chosenAnswers, //named arguments
+  });
+
+  final List<String> chosenAnswers;
 
   @override
   Widget build(BuildContext context) {
