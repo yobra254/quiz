f8d974db95744823111fa9e9d07decab8ba97af0
yobra254 <brayodhis071@gmail.com>
2023-06-06 23:06:42 +0300

    Improving UI

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
@@ -16,17 +17,34 @@ class QuestionsSummary extends StatelessWidget {
           children: summaryData.map(
             (data) {
               return Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(((data['question_index'] as int) + 1).toString()),
                   Expanded(
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(data['question'] as String),
+                        Text(
+                          data['question'] as String,
+                          style: GoogleFonts.lato(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                         SizedBox(
                           height: 5,
                         ),
-                        Text(data['user_answer'] as String),
-                        Text(data['correct_answer'] as String),
+                        Text(
+                          data['user_answer'] as String,
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 202, 171, 252),
+                          ),
+                        ),
+                        Text(data['correct_answer'] as String,
+                            style: const TextStyle(
+                              color: Color.fromARGB(255, 181, 254, 246),
+                            )),
                       ],
                     ),
                   ),
