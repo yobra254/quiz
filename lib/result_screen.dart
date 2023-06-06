aef1bc456d091e5be23fd217cdf7b03df72e6a90
yobra254 <brayodhis071@gmail.com>
2023-06-06 22:28:44 +0300

    Getting Number of correct questions

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -29,6 +29,12 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -36,13 +42,13 @@ class ResultScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'you ans x out of 20',
+            Text(
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions Correctly!!',
             ),
             const SizedBox(
               height: 30,
             ),
-            QuestionsSummary(getSummaryData()),
+            QuestionsSummary(summaryData),
             const SizedBox(
               height: 30,
             ),
